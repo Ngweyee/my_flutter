ba09e76d0efa36f6c0960c5d36b0b0126d2de4e6
ngweyeeshoon <ngweyeeshoon.1@gmail>
2018-10-05 15:28:15 +0630

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blueGrey,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Flutter Home'),
     );
   }
 }
